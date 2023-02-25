b46073b9b795be0286ed798f09b147681fd8ed41
Zack Brown <zack@zackbrown.co.uk>
2023-02-25 13:46:09 +0000

    Tidy up vector to coordinate conversion

--- Sources/Cantilever/Extensions/Vector.swift
+++ Sources/Cantilever/Extensions/Vector.swift
@@ -16,13 +16,14 @@ extension Vector {
     
     public func convert(to scale: Triangle.Scale) -> Coordinate {
         
-        let xv = x + (0.5 * scale.rawValue)
-        let yv = z + ((-.sqrt3 / 6.0) * scale.rawValue)
-        let zv = x - (0.5 * scale.rawValue)
+        let edgeLength = Double(scale.rawValue)
+        let xv = x + (0.5 * edgeLength)
+        let yv = z + ((-.sqrt3 / 6.0) * edgeLength)
+        let zv = x - (0.5 * edgeLength)
         
-        let cx = Int(floor(( 1 * xv - .sqrt3 / 3.0 * yv) / scale.rawValue))
-        let cy = Int(ceil((     .sqrt3 * 2.0 / 3.0 * yv) / scale.rawValue))
-        let cz = Int(floor((-1 * zv - .sqrt3 / 3.0 * yv) / scale.rawValue))
+        let cx = Int(floor(( 1 * xv - .sqrt3 / 3.0 * yv) / edgeLength))
+        let cy = Int(ceil((     .sqrt3 * 2.0 / 3.0 * yv) / edgeLength))
+        let cz = Int(floor((-1 * zv - .sqrt3 / 3.0 * yv) / edgeLength))
         
         return Coordinate(cx, cy, cz)
     }
